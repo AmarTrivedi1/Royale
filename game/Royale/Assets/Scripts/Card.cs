cdd686bf63ea373039d3466a87626dcbed507fcd
ChristopherGug <cgugelmeier@gmail.com>
2024-04-01 18:54:17 -0400

    Base card class added

---
+++ game/Royale/Assets/Scripts/Card.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    private string cardName;
+    private int cardCost;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Returns the name of the card
+    string getName()
+    {
+        return cardName;
+    }
+
+    //Sets the name of the card
+    void setName(string name)
+    {
+        this.cardName = name;
+    }
+
+    //Returns the cost of the card
+    int getCost()
+    {
+        return cardCost;
+    }
+
+    //Sets the cost of the card
+    void setCost(int cost)
+    {
+        this.cardCost = cost;
+    }
+}
